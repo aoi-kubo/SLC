e247bd7373e439764fb0e00c14961670de051068
aoikubo <sakubo8440@gmail.com>
2025-02-08 22:12:33 +0900

    削除ボタンの実装

--- js/main.js
+++ js/main.js
@@ -20,5 +20,41 @@ $(document).ready(function() {
           
   $modalOpenButton.on('click', openModal);				
   $modalCloseButton.on('click', closeModal);				
-  $modal.on('click', onClickOutside);				
+  $modal.on('click', onClickOutside);
 });	
+
+$(document).ready(function () {
+  let startX = 0;
+  let currentX = 0;
+  let isSwiping = false;
+
+  $(".item-contents").on("touchstart", function (e) {
+      startX = e.originalEvent.touches[0].clientX;
+      isSwiping = false;
+  });
+
+  $(".item-contents").on("touchmove", function (e) {
+      currentX = e.originalEvent.touches[0].clientX;
+      let diffX = startX - currentX;
+
+      if (diffX > 20) { // 左スワイプ
+          isSwiping = true;
+          $(".item-contents").removeClass("show-delete"); // 他のアイテムを閉じる
+          $(this).addClass("show-delete");
+      } else if (diffX < -20) { // 右スワイプで戻す
+          isSwiping = true;
+          $(this).removeClass("show-delete");
+      }
+  });
+
+  $(".item-contents").on("touchend", function () {
+      if (!isSwiping) {
+          $(this).removeClass("show-delete");
+      }
+  });
+
+  // 削除ボタンのクリックでアイテムを削除
+  $(".delete-btn").on("click", function () {
+      $(this).closest(".item-contents").remove();
+  });
+});
